0b03091b4dcf19459a733b8ca517d5b3e258c525
Mohamed Belaouad <mohamed.belaouad@wattsense.com>
2023-12-11 18:37:00 +0100

    Make sure sync wrapper builds for sqlite/postgres

--- src/sync_connection_wrapper.rs
+++ src/sync_connection_wrapper.rs
@@ -1,10 +1,12 @@
 use crate::{AnsiTransactionManager, AsyncConnection, SimpleAsyncConnection};
 use diesel::backend::Backend;
+use diesel::connection::LoadConnection;
 use diesel::query_builder::{AsQuery, QueryBuilder, QueryFragment, QueryId};
-use diesel::{Connection, ConnectionError, ConnectionResult, QueryResult};
+use diesel::sql_types::TypeMetadata;
+use diesel::{Connection, ConnectionResult, QueryResult};
 use futures_util::future::BoxFuture;
-use futures_util::stream::{BoxStream, TryStreamExt};
-use futures_util::{Future, FutureExt, StreamExt};
+use futures_util::stream::BoxStream;
+use futures_util::FutureExt;
 use std::sync::Arc;
 use std::sync::Mutex;
 use tokio::task::JoinError;
@@ -28,6 +30,36 @@ impl<C: diesel::connection::Connection> SyncConnectionWrapper<C> {
     }
 }
 
+trait WithMetadataLookup: Connection {
+    fn metadata_lookup(&mut self) -> &mut <Self::Backend as TypeMetadata>::MetadataLookup;
+}
+
+#[cfg(feature = "sqlite")]
+static mut SQLITE_METADATA_LOOKUP: () = ();
+#[cfg(feature = "sqlite")]
+impl<C> WithMetadataLookup for C
+where
+    C: diesel::connection::Connection<Backend = diesel::sqlite::Sqlite>,
+{
+    fn metadata_lookup(&mut self) -> &mut <Self::Backend as TypeMetadata>::MetadataLookup {
+        // safe since it's unit type
+        unsafe { &mut SQLITE_METADATA_LOOKUP }
+    }
+}
+
+#[cfg(feature = "postgres")]
+impl<C> WithMetadataLookup for C
+where
+    C: diesel::connection::Connection<Backend = diesel::pg::Pg>
+        + LoadConnection
+        + diesel::pg::PgMetadataLookup
+        + 'static,
+{
+    fn metadata_lookup(&mut self) -> &mut <Self::Backend as TypeMetadata>::MetadataLookup {
+        self
+    }
+}
+
 #[async_trait::async_trait]
 impl<C> SimpleAsyncConnection for SyncConnectionWrapper<C>
 where
@@ -47,12 +79,16 @@ where
 }
 
 #[async_trait::async_trait]
-impl<C> AsyncConnection for SyncConnectionWrapper<C>
+impl<'a, C> AsyncConnection for SyncConnectionWrapper<C>
 where
-    C: diesel::connection::Connection + diesel::connection::LoadConnection + 'static,
-    C: diesel::Connection<Backend = diesel::pg::Pg>,
-    <C as diesel::Connection>::Backend: std::default::Default,
+    C: diesel::connection::Connection
+        + diesel::connection::LoadConnection
+        + WithMetadataLookup
+        + 'static,
+    <C as diesel::Connection>::Backend: std::default::Default + 'static,
     <<C as diesel::Connection>::Backend as Backend>::QueryBuilder: std::default::Default,
+    <<C as diesel::Connection>::Backend as Backend>::BindCollector<'a>: std::default::Default,
+    <C as diesel::Connection>::Backend: diesel::backend::DieselReserveSpecialization,
 {
     type LoadFuture<'conn, 'query> = BoxFuture<'query, QueryResult<Self::Stream<'conn, 'query>>>;
     type ExecuteFuture<'conn, 'query> = BoxFuture<'query, QueryResult<usize>>;
@@ -72,7 +108,7 @@ where
             .map(|c| SyncConnectionWrapper::new(c))
     }
 
-    fn load<'conn, 'query, T>(&'conn mut self, source: T) -> Self::LoadFuture<'conn, 'query>
+    fn load<'conn, 'query, T>(&'conn mut self, _source: T) -> Self::LoadFuture<'conn, 'query>
     where
         T: AsQuery + 'query,
         T::Query: QueryFragment<Self::Backend> + QueryId + 'query,
@@ -87,27 +123,39 @@ where
     where
         T: QueryFragment<Self::Backend> + QueryId + 'query,
     {
-        // we explicilty descruct the query here before going into the async block
-        //
-        // That's required to remove the send bound from `T` as we have translated
-        // the query type to just a string (for the SQL) and a bunch of bytes (for the binds)
-        // which both are `Send`.
-        // We also collect the query id (essentially an integer) and the safe_to_cache flag here
-        // so there is no need to even access the query in the async block below
+        let backend = <Self as AsyncConnection>::Backend::default();
         let mut query_builder =
             <<<Self as AsyncConnection>::Backend as Backend>::QueryBuilder as Default>::default();
-        let sql = source
-            .to_sql(
-                &mut query_builder,
-                &<Self as AsyncConnection>::Backend::default(),
-            )
-            .map(|_| query_builder.finish());
-        println!("{:?}", sql);
+        let mut _bind_collector = <<<Self as AsyncConnection>::Backend as Backend>::BindCollector<
+            'a,
+        > as Default>::default();
+
+        let sql = {
+            let exclusive = self.inner.clone();
+            let mut inner = exclusive.lock().unwrap();
+            let sql = source
+                .to_sql(&mut query_builder, &backend)
+                .map(|_| query_builder.finish());
+            let _metadata_lookup = inner.metadata_lookup();
+            // We would like bind_collector to contains binds so they can be moved to the async task lower down
+            // One issue is BindCollector trait hasn't Send requirement, and its Sqlite implementation can have borrowed data
+            // A trait OwnableBinds could be implemented for BindCollector so they provide the output binds to be owned.
+            // source
+            //     .collect_binds(&mut bind_collector, metadata_lookup, &backend)
+            //     .unwrap(); // FIXME UNWRAP
+
+            println!("Generated SQL '{:?}'", sql);
+            sql
+        };
 
         let inner = self.inner.clone();
         tokio::task::spawn_blocking(move || {
+            let mut inner = inner.lock().unwrap();
+            // As we want to plug to the underlying connection, we would like to somehow build a QueryFragment that could be passed
+            // The tricky thing is it needs to be build from the string query and its bindings.
+            // One candidate could be `sql_query` and then binding the individual binds to it with its bind method
             let sql = diesel::sql_query(sql?);
-            inner.lock().unwrap().execute_returning_count(&sql)
+            inner.execute_returning_count(&sql)
         })
         .map(|fut| fut.unwrap_or_else(|e| Err(from_tokio_join_error(e))))
         .boxed()
