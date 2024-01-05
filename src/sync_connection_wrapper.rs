669a6a741eb0bc1cedcfe6a6125d05bd95426e44
Mohamed Belaouad <mohamed.belaouad@wattsense.com>
2024-01-05 17:13:48 +0100

    Test more

--- src/sync_connection_wrapper.rs
+++ src/sync_connection_wrapper.rs
@@ -51,7 +51,6 @@ where
 impl<C> WithMetadataLookup for C
 where
     C: diesel::connection::Connection<Backend = diesel::pg::Pg>
-        + LoadConnection
         + diesel::pg::PgMetadataLookup
         + 'static,
 {
@@ -79,16 +78,17 @@ where
 }
 
 #[async_trait::async_trait]
-impl<'a, C> AsyncConnection for SyncConnectionWrapper<C>
+impl<C> AsyncConnection for SyncConnectionWrapper<C>
 where
     C: diesel::connection::Connection
         + diesel::connection::LoadConnection
         + WithMetadataLookup
         + 'static,
-    <C as diesel::Connection>::Backend: std::default::Default + 'static,
+    <C as diesel::Connection>::Backend:
+        std::default::Default + diesel::backend::DieselReserveSpecialization,
     <<C as diesel::Connection>::Backend as Backend>::QueryBuilder: std::default::Default,
-    <<C as diesel::Connection>::Backend as Backend>::BindCollector<'a>: std::default::Default,
-    <C as diesel::Connection>::Backend: diesel::backend::DieselReserveSpecialization,
+    for<'a> <<C as diesel::Connection>::Backend as Backend>::BindCollector<'a>:
+        std::default::Default + MovableBindCollector<C::Backend>,
 {
     type LoadFuture<'conn, 'query> = BoxFuture<'query, QueryResult<Self::Stream<'conn, 'query>>>;
     type ExecuteFuture<'conn, 'query> = BoxFuture<'query, QueryResult<usize>>;
@@ -124,38 +124,45 @@ where
         T: QueryFragment<Self::Backend> + QueryId + 'query,
     {
         let backend = <Self as AsyncConnection>::Backend::default();
-        let mut query_builder =
-            <<<Self as AsyncConnection>::Backend as Backend>::QueryBuilder as Default>::default();
-        let mut _bind_collector = <<<Self as AsyncConnection>::Backend as Backend>::BindCollector<
-            'a,
-        > as Default>::default();
 
-        let sql = {
+        let (collect_bind_result, movable_collector) = {
             let exclusive = self.inner.clone();
             let mut inner = exclusive.lock().unwrap();
+            let mut bind_collector = <<<Self as AsyncConnection>::Backend as Backend>::BindCollector<
+                '_,
+            > as Default>::default();
+            let mut metadata_lookup = inner.metadata_lookup();
+            // value in source could be stored in bind_collector
+            // source must live at least as long as bind collector
+            let result = source.collect_binds(&mut bind_collector, &mut metadata_lookup, &backend);
+            let movable_collector = bind_collector.movable();
+
+            let mut query_builder =
+                <<<Self as AsyncConnection>::Backend as Backend>::QueryBuilder as Default>::default(
+                );
             let sql = source
                 .to_sql(&mut query_builder, &backend)
                 .map(|_| query_builder.finish());
-            let _metadata_lookup = inner.metadata_lookup();
-            // We would like bind_collector to contains binds so they can be moved to the async task lower down
-            // One issue is BindCollector trait hasn't Send requirement, and its Sqlite implementation can have borrowed data
-            // A trait OwnableBinds could be implemented for BindCollector so they provide the output binds to be owned.
-            // source
-            //     .collect_binds(&mut bind_collector, metadata_lookup, &backend)
-            //     .unwrap(); // FIXME UNWRAP
-
             println!("Generated SQL '{:?}'", sql);
-            sql
+
+            (result, movable_collector)
         };
+
+        let mut query_builder =
+            <<<Self as AsyncConnection>::Backend as Backend>::QueryBuilder as Default>::default();
+        let sql = source
+            .to_sql(&mut query_builder, &backend)
+            .map(|_| query_builder.finish());
+        println!("Generated SQL '{:?}'", sql);
+        let is_safe_to_cache_prepared = source.is_safe_to_cache_prepared(&backend);
 
         let inner = self.inner.clone();
         tokio::task::spawn_blocking(move || {
+            // collect_bind_result?;
+            let query = CollectedQuery::new(sql?, is_safe_to_cache_prepared?, movable_collector);
             let mut inner = inner.lock().unwrap();
-            // As we want to plug to the underlying connection, we would like to somehow build a QueryFragment that could be passed
-            // The tricky thing is it needs to be build from the string query and its bindings.
-            // One candidate could be `sql_query` and then binding the individual binds to it with its bind method
-            let sql = diesel::sql_query(sql?);
-            inner.execute_returning_count(&sql)
+            // inner.execute_returning_count(&query)
+            Ok(0)
         })
         .map(|fut| fut.unwrap_or_else(|e| Err(from_tokio_join_error(e))))
         .boxed()
